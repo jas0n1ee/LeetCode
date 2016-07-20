557a253f05da0d6bc6872cd6d6f553c5abba6a3a
jas0n1ee <i@jas0n1ee.me>
2016-07-19 20:53:10 -0700

    292

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include<iostream>
 using namespace std;
-
+class Solution {
+public:
+    bool canWinNim(int n) {
+        return (n%4)>0;
+    }
+};
 int main() {
+    Solution s;
+    cout << s.canWinNim(9);
     return 0;
 }
