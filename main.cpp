1ee7e5969c79d260c1ba281abd1ebbd24c34282d
jas0n1ee <i@jas0n1ee.me>
2016-07-19 20:41:41 -0700

    344

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include<iostream>
 using namespace std;
-
+class Solution {
+public:
+    string reverseString(string s) {
+        const char *ori = s.c_str();
+        int l = s.length();
+        char *reversed = new char[l+1];
+        for(int i = 0; i < l; i++)
+            reversed[i] = ori[l - 1 - i];
+        reversed[l] = '\0';
+        return string(reversed);
+    }
+};
 int main() {
+    Solution s;
+    cout << s.reverseString("hello world");
     return 0;
 }
