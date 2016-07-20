d12b7dc21392857cdcf4c459a8646b4eb896cb39
jisli <jisli@cisco.com>
2016-07-20 15:30:02 -0700

    371

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include<iostream>
 using namespace std;
-
+class Solution {
+public:
+    int getSum(int a, int b) {
+        return a+b;
+    }
+};
 int main() {
+    Solution s;
+    cout << s.getSum(3, 4);
     return 0;
 }
