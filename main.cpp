61a8ac0c864d070d353e4db4f383f11f23e20be9
jisli <jisli@cisco.com>
2016-07-20 15:12:48 -0700

    202

--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
 #include<iostream>
+#include<cmath>
 using namespace std;
-
+class Solution {
+public:
+    bool isHappy(int n) {
+        int r = 0;
+        int num = n;
+        int iter = floor(log10(n));
+        for(int i = 0; i <= iter; i++) {
+            r += (num%10) * (num%10);
+            num = num/10;
+        }
+        if (checkList[r]) return false;
+        else if (r == 1) return true;
+        else {
+            checkList[r] = 1;
+            return isHappy(r);
+        }
+    }
+    bool checkList[810] = {0};
+};
 int main() {
+    Solution s;
+    cout<< s.isHappy(2147483647);
     return 0;
 }
