441f3b2b422f310cbdf2ad4c97f84108d96303f4
jisli <jisli@cisco.com>
2016-07-20 14:16:31 -0700

    258

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include<iostream>
 using namespace std;
-
+class Solution {
+public:
+    int addDigits(int num) {
+        return (num > 9)?addDigits(num/10+num%10):num;
+    }
+};
 int main() {
+    Solution s;
+    cout<<s.addDigits(45468);
     return 0;
 }
