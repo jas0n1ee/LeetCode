1eab53649a24c9423d59c1a2b96705550ee1f307
jisli <jisli@cisco.com>
2016-07-21 12:00:32 -0700

    217

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include<iostream>
+#include<vector>
+#include<algorithm>
 using namespace std;
-
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int l = nums.size();
+        for (int i = 0; i < l-1; i++) {
+            if(nums[i] == nums[i+1])
+                return true;
+        }
+        return false;
+    }
+};
 int main() {
     return 0;
 }
