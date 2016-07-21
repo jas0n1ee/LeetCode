647886ffbe24496dacae757a80c45830386d4e51
jisli <jisli@cisco.com>
2016-07-21 15:49:19 -0700

    326

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include<iostream>
 using namespace std;
-
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        return (n>0)?((n==1)?true:isPowerOfThree(n/3) && (n%3==0)):false;
+    }
+};
 int main() {
+    Solution s;
+    cout<<s.isPowerOfThree(27);
     return 0;
 }
