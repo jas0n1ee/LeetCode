bf0604fd6017188fc09cbf7144e3b117bf0ae5ad
jisli <jisli@cisco.com>
2016-07-21 15:54:24 -0700

    231

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include<iostream>
 using namespace std;
-
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        return n>0 && 1073741824%n==0;
+    }
+};
 int main() {
     return 0;
 }
