fa3c4b6675ca2e91035635ab55b07160246bef2a
jisli <jisli@cisco.com>
2016-07-21 10:33:21 -0700

    100

--- main.cpp
+++ main.cpp
@@ -1,6 +1,29 @@
 #include<iostream>
 using namespace std;
-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (p == NULL || q == NULL)
+            return p == q;
+        else
+            return p->val == q->val && isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
+    }
+};
 int main() {
     return 0;
 }
