c8ca718d8276cca39f3097425591739a03ac62fb
jisli <jisli@cisco.com>
2016-07-21 10:37:43 -0700

    171

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include<iostream>
 using namespace std;
-
+class Solution {
+public:
+    int titleToNumber(string s) {
+        int result = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int n = s.c_str()[i] - 'A' + 1;
+            result = result*26 + n;
+        }
+        return result;
+    }
+};
 int main() {
+    Solution s;
+    cout<<s.titleToNumber("A");
     return 0;
 }
