0d3f3d19cac8e8eea7663153f2a15de96e3292cb
jisli <jisli@cisco.com>
2016-07-21 11:55:28 -0700

    169

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
 #include<iostream>
+#include<vector>
+#include<algorithm>
+#include<cmath>
 using namespace std;
-
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int major = 0, count = 0;
+        int l = nums.size();
+        for (int i = 0; i < l; i++) {
+            if(count == 0) {
+                major = nums[i];
+                count ++;
+            }
+            else if (major == nums[i])
+                count ++;
+            else
+                count --;
+        }
+        return major;
+    }
+};
 int main() {
     return 0;
 }
