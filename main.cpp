ac759a5266fbc90096fe014fefde9c2610f78b55
jisli <jisli@cisco.com>
2016-07-21 16:15:32 -0700

    263

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
 #include<iostream>
 using namespace std;
-
+class Solution {
+public:
+    bool isUgly(int num) {
+        if(num < 2)
+            return num==1;
+        else {
+            int n2 = num%2;
+            if(!n2)
+                return isUgly(num/2);
+            int n3 = num%3;
+            if(!n3)
+                return isUgly(num/3);
+            int n5 = num%5;
+            if (!n5)
+                return isUgly(num/5);
+            return false;
+        }
+    }
+};
 int main() {
+    Solution s;
+    cout<<s.isUgly(14);
     return 0;
 }
