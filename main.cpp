e54a6e3dd03d3b8f7815bc1bb2f38afd5d133337
jisli <jisli@cisco.com>
2016-07-21 15:49:50 -0700

    O(1)...

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@ using namespace std;
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        return (n>0)?((n==1)?true:isPowerOfThree(n/3) && (n%3==0)):false;
+        return n > 0 && 1162261467 % n == 0;
     }
 };
 int main() {
